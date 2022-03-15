24613f22a04c17e728e7f94e7bf8aa53e7b50fac
= <=>
2022-03-15 17:56:17 +0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # PerformanceMeter
+
+A helpful library that allows you to measure and compare the runtime of the code you write with PHP.
+
+[![Latest Stable Version](http://poser.pugx.org/initphp/performancemeter/v)](https://packagist.org/packages/initphp/performancemeter) [![Total Downloads](http://poser.pugx.org/initphp/performancemeter/downloads)](https://packagist.org/packages/initphp/performancemeter) [![Latest Unstable Version](http://poser.pugx.org/initphp/performancemeter/v/unstable)](https://packagist.org/packages/initphp/performancemeter) [![License](http://poser.pugx.org/initphp/performancemeter/license)](https://packagist.org/packages/initphp/performancemeter) [![PHP Version Require](http://poser.pugx.org/initphp/performancemeter/require/php)](https://packagist.org/packages/initphp/performancemeter)
+
+## Installation
+
+```
+composer require initphp/performancemeter
+```
+
+This is a library consisting of a single file and a single class. You can choose to manually include the `src/PerformanceMeter.php` file in the project. However, I recommend Composer to manage patches and updates more easily.
+
+## Usage
+
+```php
+require_once "../vendor/autoload.php";
+
+use InitPHP\PerformanceMeter\PerformanceMeter;
+
+PerformanceMeter::setPointer('main');
+for($i = 0; $i <= 1000; $i++){
+    usleep(10);
+}
+PerformanceMeter::setPointer('mainEnd');
+
+echo PerformanceMeter::elapsedTime('main', 'mainEnd', 3) . ' seconds passed ';
+echo PerformanceMeter::memoryUsage('main', 'mainend', 2) . ' memory used.';
+// Output : "15.204 seconds passed 0.77KB memory used."
+```
+
+## Credits
+
+- [Muhammet ŞAFAK](https://www.muhammetsafak.com.tr) <<info@muhammetsafak.com.tr>>
+
+## License
+
+Copyright &copy; 2022 [MIT License](./LICENSE)
---
+++ src/PerformanceMeter.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * PerformanceMeter.php
+ *
+ * This file is part of InitPHP.
+ *
+ * @author     Muhammet ŞAFAK <info@muhammetsafak.com.tr>
+ * @copyright  Copyright © 2022 InitPHP
+ * @license    http://initphp.github.io/license.txt  MIT
+ * @version    1.0
+ * @link       https://www.muhammetsafak.com.tr
+ */
+
+declare(strict_types=1);
+
+namespace InitPHP\PerformanceMeter;
+
+class PerformanceMeter
+{
+
+    protected static array $pointers = [];
+
+    /**
+     * Ölçüm noktası oluşturur.
+     *
+     * @param string $name
+     */
+    public static function setPointer(string $name): void
+    {
+        $mtime = explode(' ', microtime());
+        self::$pointers[strtolower($name)] = [
+            'time'      => $mtime[1] + $mtime[0],
+            'memory'    => memory_get_usage(),
+        ];
+    }
+
+    /**
+     * @see PerformansMeter::setPointer()
+     * @param string $name
+     * @return void
+     */
+    public static function mark(string $name): void
+    {
+        self::setPointer($name);
+    }
+
+    /**
+     * İki ölçüm noktası arasındaki zamanı ölçer.
+     *
+     * @param string $startPoint <p>Ölçüm için kullanılacak birinci noktanın adı.</p>
+     * @param string|null $endPoint <p>Ölçüm için kullanılacak ikinci noktanın adı. <code>null</code> ise kullanıldığı yer kabul edilir.</p>
+     * @param int $decimal <p>Sonucun kayar noktalı sayı kısmında noktadan sonra kaç hane gösterileceğini belirtir.</p>
+     * @return float
+     */
+    public static function elapsedTime(string $startPoint, ?string $endPoint = null, int $decimal = 4): float
+    {
+        $start = self::getPointer($startPoint);
+        $end = self::getPointer($endPoint);
+        return round(($start['time'] - $end['time']), $decimal);
+    }
+
+    /**
+     * İki ölçüm noktası arasındaki bellek kullanımını ölçer.
+     *
+     * @param string $startPoint <p>Ölçüm için kullanılacak birinci noktanın adı.</p>
+     * @param string|null $endPoint <p>Ölçüm için kullanılacak ikinci noktanın adı. <code>null</code> ise kullanıldığı yer kabul edilir.</p>
+     * @param int $decimal <p>Sonucun kayar noktalı sayı kısmında noktadan sonra kaç hane gösterileceğini belirtir.</p>
+     * @return string
+     */
+    public static function memoryUsage(string $startPoint, ?string $endPoint = null, int $decimal = 2): string
+    {
+        $start = self::getPointer($startPoint);
+        $end = self::getPointer($endPoint);
+        $memoryUse = $end['memory'] - $start['memory'];
+        if($memoryUse < 1048576){
+            return round(($memoryUse / 1024), $decimal) . 'KB';
+        }
+        return round(($memoryUse / 1048576), $decimal) . 'MB';
+    }
+
+    /**
+     * @param string|null $name
+     * @return array
+     */
+    protected static function getPointer(?string $name = null): array
+    {
+        if($name !== null){
+            $name = strtolower($name);
+        }
+        if($name === null || !isset(self::$pointers[$name])){
+            $mtime = explode(' ', microtime());
+            return [
+                'time'      => $mtime[1] + $mtime[0],
+                'memory'    => memory_get_usage()
+            ];
+        }
+        return self::$pointers[$name];
+    }
+}
